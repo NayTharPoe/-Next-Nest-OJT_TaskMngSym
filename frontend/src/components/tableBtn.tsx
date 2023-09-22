ba2794b571125b32ab1937285fc5f672ea1d2938
LinnAungHtet-MTM <136548606+LinnAungHtet-MTM@users.noreply.github.com>
2023-09-22 09:35:01 +0630

    feature/task_crud_ui task list page ui (still doing)

--- frontend/src/components/tableBtn.tsx
+++ frontend/src/components/tableBtn.tsx
@@ -1,60 +1,50 @@
-import { Button, styled } from '@mui/material';
-import React from 'react';
-import palette from '@/theme/palette';
+import { Button, styled } from "@mui/material";
+import React from "react";
+import palette from "@/theme/palette";
 
-const TableBtn = ({ tableBtnText } : {tableBtnText: string}) => {
-  let colorCode
-  const primaryColor = palette.primary.main
-  const secondaryColor = palette.secondary.main
-  const errorColor = palette.error.main
+const TableBtn = (props: any) => {
+  let colorCode;
+  const secondaryColor = palette.secondary.main;
+  const errorColor = palette.error.light;
 
-  switch (tableBtnText) {
-    case 'Edit':
-      colorCode = secondaryColor
+  switch (props.children) {
+    case "Edit":
+      colorCode = secondaryColor;
       break;
-    case 'Preview':
-      colorCode = primaryColor
-      break;
-    case 'Remove':
-      colorCode = errorColor
+    case "Remove":
+      colorCode = errorColor;
       break;
     default:
-      colorCode = secondaryColor
+      colorCode = secondaryColor;
       break;
   }
 
   const TableButton = styled(Button)({
-    boxShadow: 'none',
-    textTransform: 'none',
+    boxShadow: "none",
+    textTransform: "none",
     fontSize: 13,
-    padding: '3px 20px',
-    border: '1px solid',
-    borderRadius: '20px',
+    padding: "3px 20px",
+    border: "1px solid",
+    borderRadius: "20px",
     backgroundColor: colorCode,
     color: palette.text.primary,
-    borderColor: tableBtnText === "Edit" ? secondaryColor : primaryColor,
-    marginRight: '10px',
+    borderColor: props.children === "Edit" ? secondaryColor : errorColor,
+    marginRight: "10px",
     "&:hover": {
-      backgroundColor: tableBtnText === "Edit" ? secondaryColor : primaryColor,
-      borderColor: tableBtnText === "Edit" ? secondaryColor : primaryColor,
+      backgroundColor: props.children === "Edit" ? secondaryColor : errorColor,
+      borderColor: props.children === "Edit" ? secondaryColor : errorColor,
       boxShadow: "none",
     },
     "&:active": {
       boxShadow: "none",
-      backgroundColor: tableBtnText === "Edit" ? secondaryColor : primaryColor,
-      borderColor: tableBtnText === "Edit" ? secondaryColor : primaryColor,
-    }
+      backgroundColor: props.children === "Edit" ? secondaryColor : errorColor,
+      borderColor: props.children === "Edit" ? secondaryColor : errorColor,
+    },
   });
 
   return (
-    <TableButton
-      variant="contained"
-      onClick={(e) => {
-        e.stopPropagation();
-        e.preventDefault();
-      }}
-    >
-      {tableBtnText}
+    <TableButton variant="contained" {...props}>
+      {props.children}
     </TableButton>
   );
 };
