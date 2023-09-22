0516ed2f592201ecac425bc146465afd78b124cc
naytharpoe <136662096+NayTharPoe@users.noreply.github.com>
2023-09-22 17:03:27 +0630

    testing useFieldQuery

--- frontend/src/pages/report/add.tsx
+++ frontend/src/pages/report/add.tsx
@@ -1,244 +1,50 @@
-import React, { useState } from 'react';
-import { DataGrid, GridColDef } from '@mui/x-data-grid';
-import { Button, Paper, TextField, Select, MenuItem, Typography, FormHelperText } from '@mui/material';
-import palette from '@/theme/palette';
+import React, { ReactElement } from 'react';
+import MainLayout from '@/layouts/MainLayout';
 import * as yup from 'yup';
-import { useForm, Controller } from 'react-hook-form';
-import { yupResolver } from '@hookform/resolvers/yup';
+import { useForm, useFieldArray } from 'react-hook-form';
 
-const typesOptions = [
-  { value: 'review', label: 'Review' },
-  { value: 'coding', label: 'Coding' },
-  { value: 'learning', label: 'Learning' },
-];
-
-interface RowData {
-  id: number;
-  taskId: string;
-  taskTitle: string;
-  project: string;
-  percentage: string;
-  status: string;
-  types: string;
-  hour: string;
-}
-
+type FormValues = {
+  reports: {
+    taskId: string;
+    taskTitle: string;
+    project: string;
+    percentage: number;
+    types: string;
+    status: string;
+    hours: number;
+  }[];
+};
 const ReportAddPage = () => {
   const {
+    register,
     control,
     handleSubmit,
+    reset,
     formState: { errors },
-    setError,
   } = useForm({
-    resolver: yupResolver(validationSchema),
+    defaultValues: {
+      report: [
+        {
+          taskId: '',
+          taskTitle: '',
+          project: '',
+          percentage: 0,
+          types: '',
+          status: '',
+          hours: 0,
+        },
+      ],
+    },
   });
 
-  const [rows, setRows] = useState<RowData[]>([]);
-
-  const addNewRow = () => {
-    const newRow = {
-      id: rows.length + 1,
-      taskId: '',
-      taskTitle: '',
-      project: '',
-      percentage: '',
-      status: '',
-      types: '',
-      hour: '',
-    };
-
-    setRows([...rows, newRow]);
-  };
-
-  const removeRow = (id: any) => {
-    const updatedRows = rows.filter((row) => row.id !== id);
-    setRows(updatedRows);
-  };
-
-  const columns: GridColDef[] = [
-    { field: 'id', headerName: 'ID', width: 70, sortable: false, disableColumnMenu: true },
-    {
-      field: 'taskId',
-      headerName: 'Task ID',
-      width: 130,
-      renderCell: (params) => (
-        <form>
-          <Controller
-            name={`${params.row.id}.taskId`}
-            control={control}
-            defaultValue={params.row.taskId}
-            render={({ field }) => (
-              <>
-                <TextField {...field} variant="outlined" fullWidth />
-              </>
-            )}
-          />
-        </form>
-      ),
-    },
-    {
-      field: 'taskTitle',
-      headerName: 'Task Title',
-      width: 130,
-      renderCell: (params) => (
-        <form>
-          <Controller
-            name={`${params.row.id}.taskTitle`}
-            control={control}
-            defaultValue={params.row.taskTitle}
-            render={({ field }) => (
-              <>
-                <TextField {...field} variant="outlined" fullWidth />
-              </>
-            )}
-          />
-        </form>
-      ),
-    },
-    {
-      field: 'project',
-      headerName: 'Project',
-      width: 90,
-      renderCell: (params) => (
-        <form>
-          <Controller
-            name={`${params.row.id}.project`}
-            control={control}
-            defaultValue={params.row.project}
-            render={({ field }) => (
-              <>
-                <TextField {...field} variant="outlined" fullWidth />
-              </>
-            )}
-          />
-        </form>
-      ),
-    },
-    {
-      field: 'percentage',
-      headerName: 'Percentage',
-      type: 'number',
-      width: 90,
-      renderCell: (params) => (
-        <form>
-          <Controller
-            name={`${params.row.id}.percentage`}
-            control={control}
-            defaultValue={params.row.percentage}
-            render={({ field }) => (
-              <>
-                <TextField {...field} type="number" variant="outlined" fullWidth />
-              </>
-            )}
-          />
-        </form>
-      ),
-    },
-    {
-      field: 'types',
-      headerName: 'Types',
-      width: 90,
-      renderCell: (params) => (
-        <form>
-          <Controller
-            name={`${params.row.id}.types`}
-            control={control}
-            defaultValue={params.row.types}
-            render={({ field }) => (
-              <>
-                <Select {...field}>
-                  {typesOptions.map((option) => (
-                    <MenuItem key={option.value} value={option.value}>
-                      {option.label}
-                    </MenuItem>
-                  ))}
-                </Select>
-              </>
-            )}
-          />
-        </form>
-      ),
-    },
-    {
-      field: 'status',
-      headerName: 'Status',
-      width: 90,
-      renderCell: (params) => (
-        <form>
-          <Controller
-            name={`${params.row.id}.status` as keyof RowData}
-            control={control}
-            defaultValue={params.row.status}
-            render={({ field }) => (
-              <>
-                <TextField {...field} variant="outlined" fullWidth />
-              </>
-            )}
-          />
-        </form>
-      ),
-    },
-    {
-      field: 'hour',
-      headerName: 'Hour',
-      type: 'number',
-      sortable: false,
-      width: 90,
-      renderCell: (params) => (
-        <form>
-          <Controller
-            name={`${params.row.id}.hour`}
-            control={control}
-            defaultValue={params.row.hour}
-            render={({ field }) => (
-              <>
-                <TextField {...field} type="number" variant="outlined" fullWidth />
-              </>
-            )}
-          />
-        </form>
-      ),
-    },
-    {
-      field: 'actions',
-      headerName: 'Actions',
-      width: 120,
-      sortable: false,
-      renderCell: (params) => (
-        <Button variant="outlined" color="error" onClick={() => removeRow(params.row.id)}>
-          Remove
-        </Button>
-      ),
-    },
-  ];
-
-  const onSubmit = () => {
-    console.log(rows);
-  };
-
-  return (
-    <Paper sx={{ height: 400, width: '100%', backgroundColor: palette.common.white }}>
-      <Button onClick={addNewRow} variant="contained" color="primary">
-        Add Row
-      </Button>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <DataGrid
-          rows={rows}
-          columns={columns}
-          initialState={{
-            pagination: {
-              paginationModel: { page: 0, pageSize: 5 },
-            },
-          }}
-          pageSizeOptions={[5, 10]}
-          checkboxSelection={false}
-        />
-        <Button type="submit" variant="contained" color="primary" onClick={onSubmit}>
-          Submit
-        </Button>
-      </form>
-    </Paper>
-  );
+  const { fields } = useFieldArray({
+    name: 'report',
+    control,
+  });
+  return <div></div>;
 };
 
 export default ReportAddPage;
+ReportAddPage.getLayout = function getLayout(page: ReactElement) {
+  return <MainLayout>{page}</MainLayout>;
+};
