51d6ab4be98f6f7dd058c57215c9164cd9bcb51f
naytharpoe <136662096+NayTharPoe@users.noreply.github.com>
2023-09-26 09:51:20 +0630

    report create and list page

--- frontend/src/pages/report/list.tsx
+++ frontend/src/pages/report/list.tsx
@@ -1,16 +1,11 @@
 import React, { ReactElement, useState } from 'react';
 import MainLayout from '@/layouts/MainLayout';
-import { DataGrid, GridColDef, GridValueGetterParams } from '@mui/x-data-grid';
-import { Box, Button, TextField } from '@mui/material';
+import { DataGrid, GridColDef, GridToolbar, GridToolbarContainer, GridToolbarExport } from '@mui/x-data-grid';
+import { Box, Button, Grid, TextField } from '@mui/material';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 import AddNewBtn from '@/components/addNewBtn';
 import palette from '@/theme/palette';
-import {
-  TextFieldVariants,
-  FilledTextFieldProps,
-  OutlinedTextFieldProps,
-  StandardTextFieldProps,
-} from '@mui/material/TextField/TextField';
+import dayjs from 'dayjs';
 
 const columns: GridColDef[] = [
   { field: 'id', headerName: 'ID', width: 90, disableColumnMenu: true },
@@ -23,9 +18,12 @@ const columns: GridColDef[] = [
   {
     field: 'description',
     headerName: 'Description',
-    width: 280,
+    width: 480,
     sortable: false,
     disableColumnMenu: true,
+    renderCell: (params) => {
+      return <div dangerouslySetInnerHTML={{ __html: params.value }} />;
+    },
   },
   {
     field: 'reportTo',
@@ -42,16 +40,62 @@ const columns: GridColDef[] = [
 ];
 
 const rows = [
-  { id: 1, date: '2023-09-09', description: 'desc', reportTo: 'James Thong', reportBy: 'james' },
-  { id: 2, date: '2023-09-09', description: 'desc', reportTo: 'James Thong', reportBy: 'james' },
-  { id: 3, date: '2023-09-09', description: 'desc', reportTo: 'James Thong', reportBy: 'james' },
-  { id: 4, date: '2023-09-09', description: 'desc', reportTo: 'James Thong', reportBy: 'james' },
-  { id: 5, date: '2023-09-09', description: 'desc', reportTo: 'James Thong', reportBy: 'james' },
-  { id: 6, date: '2023-09-09', description: 'desc', reportTo: 'James Thong', reportBy: 'james' },
-  { id: 7, date: '2023-09-09', description: 'desc', reportTo: 'James Thong', reportBy: 'james' },
+  {
+    id: 1,
+    date: '2023-09-09',
+    description: `
+    <p>Report To: James Thong</p>
+    <p>Project: Project 01</p>
+    【実績】<br>
+    - Task 1, < 23% >, < Review >, < In Progress >, < 8hr > <br>
+    【所感】
+    <p>Problem: - Nothing</p>`,
+    reportTo: 'James Thong',
+    reportBy: 'james',
+  },
+  {
+    id: 2,
+    date: '2023-09-09',
+    description: `
+    <p>Report To: James Thong</p>
+    <p>Project: Project 01</p>
+    【実績】<br>
+    - Task 1, < 23% >, < Review >, < In Progress >, < 8hr > <br>
+    【所感】
+    <p>Problem: - Nothing</p>`,
+    reportTo: 'James Thong',
+    reportBy: 'james',
+  },
+  {
+    id: 3,
+    date: '2023-09-09',
+    description: `
+    <p>Report To: James Thong</p>
+    <p>Project: Project 01</p>
+    【実績】<br>
+    - Task 1, < 23% >, < Review >, < In Progress >, < 8hr > <br>
+    【所感】
+    <p>Problem: - Nothing</p>`,
+    reportTo: 'James Thong',
+    reportBy: 'james',
+  },
+  {
+    id: 4,
+    date: '2023-09-09',
+    description: `
+    <p>Report To: James Thong</p>
+    <p>Project: Project 01</p>
+    【実績】<br>
+    - Task 1, < 23% >, < Review >, < In Progress >, < 8hr > <br>
+    【所感】
+    <p>Problem: - Nothing</p>`,
+    reportTo: 'James Thong',
+    reportBy: 'james',
+  },
 ];
 
 const ReportListPage = () => {
+  const [dataSource, setDataSource] = useState([]);
   const [formData, setFormData] = useState({
     reportTo: '',
     reportBy: '',
@@ -73,68 +117,143 @@ const ReportListPage = () => {
     });
   };
 
+  function CustomToolbar() {
+    return (
+      <GridToolbarContainer>
+        <GridToolbarExport />
+      </GridToolbarContainer>
+    );
+  }
+
   const handleSubmit = (e: { preventDefault: () => void }) => {
     e.preventDefault();
-    // You can perform form submission logic here
     console.log('Form Data Submitted:', formData);
+    const formattedDate = dayjs(formData.selectedDate).format('YYYY-MM-DD') || 'null';
+    const reportTo = formData.reportTo ? formData.reportTo : 'null';
+    const reportBy = formData.reportBy ? formData.reportBy : 'null';
+    const api = `http://localhost:3000/reports/list?reportTo=${reportTo}&reportBy=${reportBy}&date=${formattedDate}`;
+
+    fetch(api)
+      .then((res) => res.json())
+      .then((data) => {
+        setDataSource(data);
+      });
   };
 
   return (
     <Box sx={{ height: 400, width: '100%' }}>
-      <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', mb: 4 }}>
-        <form onSubmit={handleSubmit}>
-          <Box sx={{ display: 'flex', gap: 2, alignItems: 'center' }}>
-            <TextField
-              name="reportTo"
-              id="report-to"
-              value={formData.reportTo}
-              onChange={handleInputChange}
-              placeholder="Report To"
-              variant="outlined"
-              sx={{
-                '.MuiOutlinedInput-input': {
-                  p: '11.5px 14px',
-                },
-              }}
-            />
-            <TextField
-              name="reportBy"
-              id="report-by"
-              value={formData.reportBy}
-              onChange={handleInputChange}
-              placeholder="Report By"
-              variant="outlined"
-              sx={{
-                '.MuiOutlinedInput-input': {
-                  p: '11.5px 14px',
-                },
-              }}
-            />
-            {/* <DatePicker
-              name="selectedDate"
-              value={formData.selectedDate}
-              onChange={handleDateChange}
-              renderInput={(params: any) => <TextField {...params} variant="outlined" />}
-              margin="normal"
-            /> */}
-            <Button variant="contained" size="large" type="submit" onClick={handleSubmit}>
-              Search
-            </Button>
-          </Box>
-        </form>
-        <AddNewBtn AddNewBtnText="Add Report" path="/report/add" />
+      <Grid
+        container
+        spacing={2}
+        sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', mb: 4 }}
+      >
+        <Grid item>
+          <form onSubmit={handleSubmit}>
+            <Box sx={{ display: 'flex', gap: 2, alignItems: 'center' }}>
+              <TextField
+                name="reportTo"
+                id="report-to"
+                value={formData.reportTo}
+                onChange={handleInputChange}
+                placeholder="Report To"
+                variant="outlined"
+                sx={{
+                  '.MuiInputBase-root': {
+                    borderRadius: '.4rem',
+                  },
+                  '.MuiOutlinedInput-input': {
+                    p: '11.5px 14px',
+                  },
+                }}
+              />
+              <TextField
+                name="reportBy"
+                id="report-by"
+                value={formData.reportBy}
+                onChange={handleInputChange}
+                placeholder="Report By"
+                variant="outlined"
+                sx={{
+                  '.MuiInputBase-root': {
+                    borderRadius: '.4rem',
+                  },
+                  '.MuiOutlinedInput-input': {
+                    p: '11.5px 14px',
+                    borderRadius: '.7rem',
+                  },
+                }}
+              />
+              <DatePicker
+                value={formData.selectedDate}
+                onChange={(newValue) => handleDateChange(newValue)}
+                sx={{
+                  '.MuiInputBase-root': {
+                    borderRadius: '.4rem',
+                  },
+                  '.MuiOutlinedInput-input': {
+                    p: '11.5px 14px',
+                    borderRadius: '.7rem',
+                  },
+                }}
+              />
+              <Button
+                variant="contained"
+                size="large"
+                type="submit"
+                onClick={handleSubmit}
+                sx={{
+                  backgroundColor: palette.primary.main,
+                  color: palette.text.primary,
+                  borderRadius: '.7rem',
+                  '&:hover': {
+                    backgroundColor: palette.primary.main,
+                    boxShadow: 'none',
+                  },
+                }}
+              >
+                Search
+              </Button>
+              {/* <Button
+                variant="contained"
+                size="large"
+                sx={{
+                  backgroundColor: palette.secondary.main,
+                  color: palette.text.primary,
+                  borderRadius: '.7rem',
+                  '&:hover': {
+                    backgroundColor: palette.secondary.main,
+                    boxShadow: 'none',
+                  },
+                }}
+              >
+                Download CSV
+              </Button> */}
+            </Box>
+          </form>
+        </Grid>
+        <Grid item>
+          <AddNewBtn AddNewBtnText="Add Report" path="/report/add" />
+        </Grid>
+      </Grid>
+      <Box sx={{ height: 550 }}>
+        <DataGrid
+          rows={rows}
+          columns={columns}
+          getRowHeight={() => 'auto'}
+          initialState={{
+            pagination: {
+              paginationModel: { page: 0, pageSize: 5 },
+            },
+          }}
+          pageSizeOptions={[5, 10]}
+          slots={{ toolbar: CustomToolbar }}
+          sx={{
+            backgroundColor: palette.common.white,
+            borderRadius: '.7rem',
+            '.MuiDataGrid-cell': { py: '20px' },
+          }}
+        />
       </Box>
-      <DataGrid
-        rows={rows}
-        columns={columns}
-        initialState={{
-          pagination: {
-            paginationModel: { page: 0, pageSize: 5 },
-          },
-        }}
-        pageSizeOptions={[5, 10]}
-        sx={{ backgroundColor: palette.common.white, borderRadius: '.7rem' }}
-      />
     </Box>
   );
 };
