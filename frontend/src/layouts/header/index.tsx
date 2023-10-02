f4429dc35d5893dd1b1459ad8669a82e20741916
naytharpoe <136662096+NayTharPoe@users.noreply.github.com>
2023-10-02 10:14:33 +0630

    added noti ui

--- frontend/src/layouts/header/index.tsx
+++ frontend/src/layouts/header/index.tsx
@@ -1,84 +1,56 @@
-import { useState, useEffect } from "react";
-import { Box, Paper } from "@mui/material";
-import IconButton from "@mui/material/IconButton";
-import Tooltip from "@mui/material/Tooltip";
-import Avatar from "@mui/material/Avatar";
-import Menu from "@mui/material/Menu";
-import MenuItem from "@mui/material/MenuItem";
-import Typography from "@mui/material/Typography";
-import Badge from "@mui/material/Badge";
-import NotificationsIcon from "@mui/icons-material/Notifications";
-import AppBar from "@mui/material/AppBar";
-import Tabs from "@mui/material/Tabs";
-import Tab from "@mui/material/Tab";
-// import SwipeableViews from 'react-swipeable-views';
-import { useTheme } from "@mui/material/styles";
-import palette from "@/theme/palette";
-import { useRouter } from "next/router";
-
-interface TabPanelProps {
-  children?: React.ReactNode;
-  dir?: string;
-  index: number;
-  value: number;
-}
-
-function TabPanel(props: TabPanelProps) {
-  const { children, value, index, ...other } = props;
-
-  return (
-    <div
-      role="tabpanel"
-      hidden={value !== index}
-      id={`full-width-tabpanel-${index}`}
-      aria-labelledby={`full-width-tab-${index}`}
-      {...other}
-    >
-      {value === index && (
-        <Box sx={{ p: 2 }}>
-          <Typography>{children}</Typography>
-        </Box>
-      )}
-    </div>
-  );
-}
-
-function a11yProps(index: number) {
-  return {
-    id: `full-width-tab-${index}`,
-    "aria-controls": `full-width-tabpanel-${index}`,
-  };
-}
+import { useState, useEffect } from 'react';
+import {
+  Box,
+  Chip,
+  Divider,
+  List,
+  ListItem,
+  ListItemAvatar,
+  ListItemButton,
+  ListItemText,
+  Paper,
+} from '@mui/material';
+import IconButton from '@mui/material/IconButton';
+import Tooltip from '@mui/material/Tooltip';
+import Avatar from '@mui/material/Avatar';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import Typography from '@mui/material/Typography';
+import Badge from '@mui/material/Badge';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import { useTheme } from '@mui/material/styles';
+import palette from '@/theme/palette';
+import { useRouter } from 'next/router';
 
 function notificationsLabel(count: number) {
   if (count === 0) {
-    return "no notifications";
+    return 'no notifications';
   }
   if (count > 99) {
-    return "more than 99 notifications";
+    return 'more than 99 notifications';
   }
   return `${count} notifications`;
 }
 
 const Header = () => {
+  const [showMessageBox, setShowMessageBox] = useState(false);
   const [anchorElUser, setAnchorElUser] = useState<null | HTMLElement>(null);
-  const [anchorElMenu, setAnchorElMenu] = useState<null | HTMLElement>(null);
   const theme = useTheme();
   const [value, setValue] = useState(0);
 
   const logoutApi = () => {
-    localStorage.removeItem("user");
-    router.push("/auth/login");
+    localStorage.removeItem('user');
+    router.push('/auth/login');
     setAnchorElUser(null);
   };
 
   const handleChangePassword = () => {
-    router.push("/auth/change-password");
+    router.push('/auth/change-password');
     setAnchorElUser(null);
   };
 
   const handleProfile = () => {
-    const loginUser = localStorage.getItem("user");
+    const loginUser = localStorage.getItem('user');
     if (loginUser) {
       const { _id } = JSON.parse(loginUser);
       router.push(`employee/detail/${_id}`);
@@ -94,37 +66,25 @@ const Header = () => {
     setAnchorElUser(null);
   };
 
-  const handleOpenMessageMenu = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorElMenu(event?.currentTarget);
-  };
-
-  const handleCloseMessageMenu = () => {
-    setAnchorElMenu(null);
-  };
-
-  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
-    setValue(newValue);
-  };
-
-  const handleChangeIndex = (index: number) => {
-    setValue(index);
+  const handleNotificationBox = () => {
+    setShowMessageBox((prevShowMessageBox) => !prevShowMessageBox);
   };
 
   const router = useRouter();
-  const routePath = router.pathname.split("/")[1];
-  let pageTitle = "";
+  const routePath = router.pathname.split('/')[1];
+  let pageTitle = '';
   switch (routePath) {
-    case "project":
-      pageTitle = "Projects";
+    case 'project':
+      pageTitle = 'Projects';
       break;
-    case "employee":
-      pageTitle = "Employees";
+    case 'employee':
+      pageTitle = 'Employees';
       break;
-    case "task":
-      pageTitle = "Tasks";
+    case 'task':
+      pageTitle = 'Tasks';
       break;
-    case "report":
-      pageTitle = "Reports";
+    case 'report':
+      pageTitle = 'Reports';
       break;
     default:
       break;
@@ -132,12 +92,12 @@ const Header = () => {
   return (
     <Box
       sx={{
-        width: "100%",
-        display: { xs: "none", md: "flex", gap: 15 },
+        width: '100%',
+        display: { xs: 'none', md: 'flex', gap: 15 },
         mt: 3,
         mb: 3,
-        justifyContent: "space-between",
-        alignItems: "center",
+        justifyContent: 'space-between',
+        alignItems: 'center',
       }}
     >
       <Box>
@@ -145,64 +105,182 @@ const Header = () => {
           {pageTitle}
         </Typography>
       </Box>
-      <Box sx={{ width: "max-content", display: "flex", gap: 3 }}>
-        {/* notification item */}
-        <Box>
-          <IconButton
-            aria-label={notificationsLabel(100)}
-            onClick={handleOpenMessageMenu}
-          >
+      <Box sx={{ width: 'max-content', display: 'flex', gap: 3 }}>
+        <Box sx={{ position: 'relative' }}>
+          <IconButton aria-label={notificationsLabel(100)} onClick={handleNotificationBox}>
             <Badge badgeContent={100} color="error">
               <NotificationsIcon />
             </Badge>
           </IconButton>
-          <Menu
-            sx={{ mt: "45px", boxShadow: 0 }}
-            id="noti-appbar"
-            anchorEl={anchorElMenu}
-            anchorOrigin={{
-              vertical: "top",
-              horizontal: "right",
-            }}
-            keepMounted
-            transformOrigin={{
-              vertical: "top",
-              horizontal: "right",
-            }}
-            open={Boolean(anchorElMenu)}
-            onClose={handleCloseMessageMenu}
-          >
-            <Box sx={{ width: 370 }}>
-              <AppBar position="static" sx={{ backgroundColor: "transparent" }}>
-                <Tabs
-                  value={value}
-                  onChange={handleChange}
-                  indicatorColor="secondary"
-                  textColor="inherit"
-                  variant="fullWidth"
-                  sx={{
-                    backgroundColor: (theme: any) =>
-                      `${theme.palette.background["100"]}`,
-                  }}
-                >
-                  <Tab label="Reports" {...a11yProps(0)} />
-                  <Tab label="Tasks" {...a11yProps(1)} />
-                </Tabs>
-              </AppBar>
-              {/* <SwipeableViews
-                axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
-                index={value}
-                onChangeIndex={handleChangeIndex}
-              >
-                <TabPanel value={value} index={0} dir={theme.direction}>
-                  Reports Message
-                </TabPanel>
-                <TabPanel value={value} index={1} dir={theme.direction}>
-                  Tasks Message
-                </TabPanel>
-              </SwipeableViews> */}
+          {/* notification item */}
+          {showMessageBox && (
+            <Box
+              sx={{
+                position: 'absolute',
+                top: 50,
+                left: -320,
+                width: 360,
+                bgcolor: palette.common.white,
+                borderRadius: '.75rem',
+                p: 1,
+                pt: 0,
+              }}
+            >
+              <Box sx={{ display: 'flex', justifyContent: 'space-between', p: 2 }}>
+                <Typography variant="h4" color={palette.text.primary}>
+                  Notification
+                </Typography>
+                <Chip label="New 16" size="small" color="info" variant="filled" />
+              </Box>
+              <Divider />
+              <List component="nav" sx={{ overflow: 'auto', maxHeight: 360 }}>
+                <ListItem alignItems="flex-start">
+                  <ListItemButton sx={{ flexDirection: 'column' }}>
+                    <Box sx={{ display: 'flex', alignItems: 'center' }}>
+                      <ListItemAvatar>
+                        <Avatar
+                          alt="profile pics"
+                          src="https://minimal-kit-react.vercel.app/assets/images/avatars/avatar_default.jpg"
+                        />
+                      </ListItemAvatar>
+                      <ListItemText
+                        sx={{ alignItems: 'center' }}
+                        primary={
+                          <Typography sx={{ color: palette.text.primary, fontWeight: '600' }}>
+                            Reports
+                          </Typography>
+                        }
+                        secondary={
+                          <>
+                            <Typography
+                              sx={{ display: 'inline' }}
+                              component="span"
+                              variant="body2"
+                              color="text.primary"
+                            >
+                              Ali Connors
+                            </Typography>
+                            {" — I'll be in your neighborhood doing errands this…"}
+                          </>
+                        }
+                      />
+                    </Box>
+                    <Typography sx={{ color: palette.text.primary, fontSize: '0.75rem', marginLeft: 'auto' }}>
+                      2 days ago
+                    </Typography>
+                  </ListItemButton>
+                </ListItem>
+                <ListItem alignItems="flex-start">
+                  <ListItemButton sx={{ flexDirection: 'column' }}>
+                    <Box sx={{ display: 'flex', alignItems: 'center' }}>
+                      <ListItemAvatar>
+                        <Avatar
+                          alt="profile pics"
+                          src="https://minimal-kit-react.vercel.app/assets/images/avatars/avatar_default.jpg"
+                        />
+                      </ListItemAvatar>
+                      <ListItemText
+                        sx={{ alignItems: 'center' }}
+                        primary={
+                          <Typography sx={{ color: palette.text.primary, fontWeight: '600' }}>
+                            Reports
+                          </Typography>
+                        }
+                        secondary={
+                          <>
+                            <Typography
+                              sx={{ display: 'inline' }}
+                              component="span"
+                              variant="body2"
+                              color="text.primary"
+                            >
+                              Ali Connors
+                            </Typography>
+                            {" — I'll be in your neighborhood doing errands this…"}
+                          </>
+                        }
+                      />
+                    </Box>
+                    <Typography sx={{ color: palette.text.primary, fontSize: '0.75rem', marginLeft: 'auto' }}>
+                      2 days ago
+                    </Typography>
+                  </ListItemButton>
+                </ListItem>
+                <ListItem alignItems="flex-start">
+                  <ListItemButton sx={{ flexDirection: 'column' }}>
+                    <Box sx={{ display: 'flex', alignItems: 'center' }}>
+                      <ListItemAvatar>
+                        <Avatar
+                          alt="profile pics"
+                          src="https://minimal-kit-react.vercel.app/assets/images/avatars/avatar_default.jpg"
+                        />
+                      </ListItemAvatar>
+                      <ListItemText
+                        sx={{ alignItems: 'center' }}
+                        primary={
+                          <Typography sx={{ color: palette.text.primary, fontWeight: '600' }}>
+                            Reports
+                          </Typography>
+                        }
+                        secondary={
+                          <>
+                            <Typography
+                              sx={{ display: 'inline' }}
+                              component="span"
+                              variant="body2"
+                              color="text.primary"
+                            >
+                              Ali Connors
+                            </Typography>
+                            {" — I'll be in your neighborhood doing errands this…"}
+                          </>
+                        }
+                      />
+                    </Box>
+                    <Typography sx={{ color: palette.text.primary, fontSize: '0.75rem', marginLeft: 'auto' }}>
+                      2 days ago
+                    </Typography>
+                  </ListItemButton>
+                </ListItem>
+                <ListItem alignItems="flex-start">
+                  <ListItemButton sx={{ flexDirection: 'column' }}>
+                    <Box sx={{ display: 'flex', alignItems: 'center' }}>
+                      <ListItemAvatar>
+                        <Avatar
+                          alt="profile pics"
+                          src="https://minimal-kit-react.vercel.app/assets/images/avatars/avatar_default.jpg"
+                        />
+                      </ListItemAvatar>
+                      <ListItemText
+                        sx={{ alignItems: 'center' }}
+                        primary={
+                          <Typography sx={{ color: palette.text.primary, fontWeight: '600' }}>
+                            Reports
+                          </Typography>
+                        }
+                        secondary={
+                          <>
+                            <Typography
+                              sx={{ display: 'inline' }}
+                              component="span"
+                              variant="body2"
+                              color="text.primary"
+                            >
+                              Ali Connors
+                            </Typography>
+                            {" — I'll be in your neighborhood doing errands this…"}
+                          </>
+                        }
+                      />
+                    </Box>
+                    <Typography sx={{ color: palette.text.primary, fontSize: '0.75rem', marginLeft: 'auto' }}>
+                      2 days ago
+                    </Typography>
+                  </ListItemButton>
+                </ListItem>
+              </List>
             </Box>
-          </Menu>
+          )}
         </Box>
         {/* profile item */}
         <Box>
@@ -215,26 +293,21 @@ const Header = () => {
             </IconButton>
           </Tooltip>
           <Menu
-            sx={{ mt: "45px" }}
+            sx={{ mt: '45px' }}
             id="menu-appbar"
             anchorEl={anchorElUser}
             anchorOrigin={{
-              vertical: "top",
-              horizontal: "right",
+              vertical: 'top',
+              horizontal: 'right',
             }}
             keepMounted
             transformOrigin={{
-              vertical: "top",
-              horizontal: "right",
+              vertical: 'top',
+              horizontal: 'right',
             }}
             open={Boolean(anchorElUser)}
             onClose={handleCloseUserMenu}
           >
-            {/* {settings.map((setting) => (
-              <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                <Typography textAlign="center">{setting}</Typography>
-              </MenuItem>
-            ))} */}
             <MenuItem onClick={handleProfile}>Profile</MenuItem>
             <MenuItem onClick={handleChangePassword}>Change Password</MenuItem>
             <MenuItem onClick={logoutApi}>Logout</MenuItem>
